39da24f7d269368302cb0c1bccaedb5286881e16
NCInside <nchristian02@student.ciputra.ac.id>
2023-05-07 21:17:52 +0700

    Initial Commit

---
+++ Chase/ChaseApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChaseApp.swift
+//  Chase
+//
+//  Created by MacBook Pro on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChaseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
